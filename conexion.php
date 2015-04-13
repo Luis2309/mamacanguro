0e3a7d6fa649d39c59ef3506666977709c853af5
Cristian Castro Del Carpio <xtiancastro1993@gmail.com>
2015-04-13 17:24:02 -0500

    Archivo de conexión (casi final)
    PHP de conexión

--- conexion.php
+++ conexion.php
@@ -1,8 +1,12 @@
 <?
-	if ($Conexion = mysqli_connect("192.168.1.12","mamacanguro","clinica","bdmaca")){
-		// echo "EXITO: Conexion realizada";
-	}
-	else {
-		// echo "ERROR: Conexion no realizada";
-	}
+	$ip = "192.168.1.12";
+
+	$Conexion = mysqli_connect($ip,"mamacanguro","clinica","bdmaca")
+
+	/*
+	- Para incluir el archivo dentro de código php utilizar la sentencia:
+	  include("conexion.php");
+	- Para incluir el archivo dentro de código html utilizar la sentencia:
+	  <php include("conexion.php"); ?>
+	*/
 ?>
