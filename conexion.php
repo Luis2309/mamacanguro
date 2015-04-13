f91d29ed8de555d3c772937b8e68c715ba9d4b77
Cristian Castro Del Carpio <xtiancastro1993@gmail.com>
2015-04-13 17:06:06 -0500

    PHP de conexión
    Este archivo es para la conexión a la Base de Datos

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?
+	if ($Conexion = mysqli_connect("192.168.1.12","mamacanguro","clinica","bdmaca")){
+		// echo "EXITO: Conexion realizada";
+	}
+	else {
+		// echo "ERROR: Conexion no realizada";
+	}
+?>
