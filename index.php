14085d5cdf8d4f5451b3871d396f6e16770fdb93
Luis2309 <luisjfr2309@gmail.com>
2015-04-13 20:05:36 -0500

    Estructura del proyecto.. a programar XD

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta charset="utf-8">
-	<title>Mamá Canguro</title>
-	<meta name="generator" content="Bootply" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-	<!--[if lt IE 9]>
-	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<link href="assets/css/signin.css" rel="stylesheet">
-	<link href="assets/css/styles.css" rel="stylesheet">
-	<link rel="icon" type="image/png" href="assets/images/icon.ico" />
+  <head>
+    <? php include('../layout/base.html'); ?>    
 </head>
 <body>
 	<!--login modal-->
